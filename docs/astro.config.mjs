8c3f766bc04c102f786c4ef3c56afc53d28c856e
rzmk <30333942+rzmk@users.noreply.github.com>
2024-07-07 01:10:38 -0400

    build: remove tailwindcss but add to docs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        starlight({
-            title: "File Converter Tool",
-            social: {
-                github: "https://github.com/rzmk/fct",
-            },
-        }),
-    ],
+  integrations: [starlight({
+    title: "File Converter Tool",
+    social: {
+      github: "https://github.com/rzmk/fct"
+    }
+  }), tailwind()]
 });
